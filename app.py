f80c836d0a242c9ab3b49fa885869081192d9e21
Ankit Kumar <41320139+ankit19988@users.noreply.github.com>
2019-03-10 15:26:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-server = app.server
 import sqlite3
 def getData():
 	conn=sqlite3.connect('E:\\MyProgress\\Curie Labs\\webserver\\sensorsData.db')
@@ -21,4 +20,4 @@ def index():
 	}
 	return render_template('index.html', **templateData)
 if __name__ == "__main__":
-   app.run_server(debug=True)
+   app.run(debug=True)
